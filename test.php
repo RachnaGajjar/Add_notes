4c04c1128b2165ee258f946aa9ea6bf9d4518551
Harikrushna <adiechahari@gmail.com>
2021-04-30 23:52:09 +0530

    Update test.php

--- test.php
+++ test.php
@@ -2,4 +2,9 @@
 
 echo "DB_URL: " . getenv('DATABASE_URL');
 
+$conn = pg_connect(getenv('DATABASE_URL'));
+$result = pg_query($conn, "select * from pg_stat_activity");
+var_dump(pg_fetch_all($result));
+
+
 ?>
